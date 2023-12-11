89fa497e2d2611feabd9dd524656894c5168930e
IPPTstudent11 <147137984+IPPTstudent11@users.noreply.github.com>
2023-12-11 22:58:11 +0200

    Update метод частоколу.py

--- метод частоколу.py
+++ метод частоколу.py
@@ -4,16 +4,18 @@ window = tk.Tk()
 label = tk.Label()
 label.pack()
 
-entry = tk.Entry(fg='black', bg='grey', width=10)
+entry = tk.Entry(fg='black', bg='grey', width=100, font=("Arial", 18))
 entry.pack()
-entry1 = tk.Entry(fg='black', bg='grey', width=10)
+entry1 = tk.Entry(fg='black', bg='grey', width=100, font=("Arial", 18))
 entry1.pack()
 
-
+result_label = tk.Label(window, text="", fg="black", font=("Arial", 18))  # Зміна розміру шрифту
+result_label.pack()
 
 def click():
-    script = ""  # Global variable
-    key =int(entry1.get())
+    global script
+    script = ""
+    key = int(entry1.get())
     text = entry.get()
     list_text = list(text)
     length = len(list_text)
@@ -29,12 +31,13 @@ def click():
             count += 1
         key -= 1
 
-    print(script)
+    result_label.configure(text=script)  # Оновлення тексту у вікні з результатом
 
 button = tk.Button(
     text="chpoink",
-    width=15,
-    height=5,
+    width=100,
+    height=50,
+    font=("Arial", 18),
     bg="red",
     fg="white",
     command=click
